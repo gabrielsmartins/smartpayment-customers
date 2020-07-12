8b82140879bdb17140f571dfff056addff553946
Gabriel Martins <ga.smartins94@gmail.com>
2020-07-12 18:23:06 -0300

    Configurando Spring Boot Maven Plugin

--- adapters/web/target/generated-sources/annotations/br/gabrielsmartins/smartpayment/adapters/web/mapper/customers/CustomerWebMapperImpl.java
+++ adapters/web/target/generated-sources/annotations/br/gabrielsmartins/smartpayment/adapters/web/mapper/customers/CustomerWebMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-07-12T17:40:45-0300",
+    date = "2020-07-12T18:20:28-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (Ubuntu)"
 )
 @Component
