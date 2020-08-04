5886ccd9b539f7ebb5f9383f90f7f2a4f921dd96
Gabriel Martins <ga.smartins94@gmail.com>
2020-08-03 22:38:21 -0300

    Adicionando Paraemtros OpenTracing

--- adapters/persistence/target/generated-sources/annotations/br/gabrielsmartins/smartpayment/adapters/persistence/mapper/customers/CustomerPersistenceMapperImpl.java
+++ adapters/persistence/target/generated-sources/annotations/br/gabrielsmartins/smartpayment/adapters/persistence/mapper/customers/CustomerPersistenceMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-03T22:30:27-0300",
+    date = "2020-08-03T22:36:22-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Ubuntu)"
 )
 @Component
